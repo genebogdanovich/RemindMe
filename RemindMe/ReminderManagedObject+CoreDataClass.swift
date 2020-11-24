bff0900670d4717f43bb57bb3329e25f345de67e
Gene Bogdanovich <genebogdanovich@gmail.com>
2020-11-24 09:29:41 +0300

    Add CoreData model.

---
+++ RemindMe/ReminderManagedObject+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderManagedObject+CoreDataClass.swift
+//  RemindMe
+//
+//  Created by Gene Bogdanovich on 24.11.20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ReminderManagedObject)
+public class ReminderManagedObject: NSManagedObject {
+
+}
---
+++ RemindMe/ReminderManagedObject+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  ReminderManagedObject+CoreDataProperties.swift
+//  RemindMe
+//
+//  Created by Gene Bogdanovich on 24.11.20.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ReminderManagedObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ReminderManagedObject> {
+        return NSFetchRequest<ReminderManagedObject>(entityName: "ReminderManagedObject")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var isCompleted: Bool
+    @NSManaged public var note: String?
+    @NSManaged public var url: String?
+
+}
+
+extension ReminderManagedObject : Identifiable {
+
+}
