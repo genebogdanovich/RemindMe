1f3f5d9bdcb0a6216327b76b00ea4c56c879104d
Gene Bogdanovich <genebogdanovich@gmail.com>
2020-11-29 18:38:17 +0300

    Add local user notifications functionality.

--- RemindMe/View Models/DetailReminderViewModel.swift
+++ RemindMe/View Models/DetailReminderViewModel.swift
@@ -35,6 +35,9 @@ final class DetailReminderViewModel: ObservableObject {
 extension DetailReminderViewModel: DetailReminderViewModelProtocol {
     func addNew(_ reminder: Reminder) {
         
+        // Schedule notification
+        
+        createLocalNotification(withTitle: reminder.name, subtitle: reminder.note, date: reminder.date, id: reminder.id)
 
         dataManager.add(Reminder(
                             id: reminder.id,
@@ -46,5 +49,18 @@ extension DetailReminderViewModel: DetailReminderViewModelProtocol {
         
     }
     
-    
+    private func createLocalNotification(withTitle title: String, subtitle: String?, date: Date, id: UUID) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle ?? ""
+        content.sound = .default
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.hour, .minute, .day, .month], from: date), repeats: false)
+        
+        let request = UNNotificationRequest(identifier: id.uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { _ in
+            // TODO: Error handling.
+        })
+    }
 }
